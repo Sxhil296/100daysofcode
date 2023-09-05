ee782157363e81eaa60a1cd7789076fa5143dafc
Sahil <leviack629@gmail.com>
2023-09-05 15:41:25 +0530

    JS Basics

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,27 +1,81 @@
-
 //variables
-let age = 24
-console.log(age)
+// let age = 24;
+// console.log(age);
 
-const salary = 35000
-console.log(salary)
-
+// const salary = 35000;
+// console.log(salary);
 
 //data types
-const name = 'Emir' //string
-console.log(name)
+// const name = "Emir"; //string
+// console.log(name);
 
-const number = 3476 //number
-console.log(number)
+// const number = 3476; //number
+// console.log(number);
 
-const isMale = true //boolean
-console.log(isMale)
+// const isMale = true; //boolean
+// console.log(isMale);
 
-let sum //undefined
-console.log(sum)
+// let sum; //undefined
+// console.log(sum);
 
-const result = undefined //undefined
+// const result = undefined; //undefined
 
-const  data = null
-console.log(data)
+// const data = null;
+// console.log(data);
+
+//objects
+// const person = {
+//   firstName: "Bruce",
+//   lastName: "Wayne",
+//   age: 30,
+// };
+// console.log(person);
+// console.log(person.firstName)
+
+
+//array
+// const oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[0])
+
+
+//operators
+
+// let x = 10
+// let y = 5
+// console.log(x % y)
+// console.log(x+y)
+// console.log(x-y)
+// console.log(++x)
+// console.log(--y)
+// console.log(x*y)
+// console.log(x / y)
+
+
+// console.log(x == y)
+// console.log(x > y)
+// console.log(x < y)
+// console.log(x >= y)
+// console.log(x <= y)
+
+// const isValid = x > 8 && 8 > y
+// const isValid = x > 8 || 8 > y
+// console.log(isValid)
+
+// const isValid = true
+// console.log(!isValid)
+
+// console.log('Bruce ' + 'Wayne')
+
+// const isEven = 10 % 2 === 0 ? "Even" : "Odd"
+// console.log(isEven)
+
+// console.log(2 + '3')
+// console.log(true + '3')
+// console.log(true + 3)
+// console.log(4 - false)
+// console.log(5 - null)
+// console.log('Emir' - 'Levi')
+
+
+// console.log(Number('6'))
 
