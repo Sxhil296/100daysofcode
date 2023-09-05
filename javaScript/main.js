c2fdeb9b161bf0187568d7923e06d468724ef5da
Sahil <leviack629@gmail.com>
2023-09-05 15:15:07 +0530

    JS Basics

---
+++ javaScript/main.js
@@ -0,0 +1,27 @@
+
+//variables
+let age = 24
+console.log(age)
+
+const salary = 35000
+console.log(salary)
+
+
+//data types
+const name = 'Emir' //string
+console.log(name)
+
+const number = 3476 //number
+console.log(number)
+
+const isMale = true //boolean
+console.log(isMale)
+
+let sum //undefined
+console.log(sum)
+
+const result = undefined //undefined
+
+const  data = null
+console.log(data)
+
