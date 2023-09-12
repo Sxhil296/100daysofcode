356d54e6abb4d1e640b33edba03484d3dfe4f2e1
Sahil <leviack629@gmail.com>
2023-09-12 16:23:15 +0530

    DOM Basics

---
+++ DOMBasics/app.js
@@ -0,0 +1,36 @@
+// const messageEl = document.getElementById('message')
+// messageEl.innerText = "Get element by ID"
+// messageEl.addEventListener('mouseover', () => {
+//     alert('Hello, DOM!')
+// })
+
+
+
+
+// const btn = document.getElementById('btnRate')
+// const outputEL = document.getElementById("output")
+
+// btn.addEventListener('click', () => {
+// let rates = document.getElementsByName('rate')
+// rates.forEach((rate) => {
+//     if(rate.checked){
+//         outputEL.innerText = `You selected : ${rate.value}`
+//     }
+// })
+// })
+
+
+// const btnEl = document.getElementById('btnCount')
+// btnEl.addEventListener('click', () => {
+//     let heading = document.getElementsByTagName('h2')
+//     alert(`The number of H2 tags are : ${heading.length}`)
+// })
+
+
+
+
+// let menuEl = document.getElementById('menu')
+// let itemsEl = document.getElementsByClassName('item')
+
+// let dataEl = [].map.call(itemsEl, item => item.textContent)
+// console.log(dataEl);
